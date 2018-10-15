de11006b3ca3c452ce32a02417c675e8859e54e9
takabatoshiki <takabatoshiki@gmail.com>
2018-10-16 07:59:49 +0900

    add massages_params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,14 @@ class MessagesController < ApplicationController
   end
 
   def create
+
   end
 
   private
+    def messages_params
+      params.require(:message).permit(:content, :image).merge(user_id: current_user)
+    end
+
     def set_group
       @group = Group.find(params[:group_id])
     end
