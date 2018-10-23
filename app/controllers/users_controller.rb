2a585fe24a93cff28248b39d2f04add883838882
takabatoshiki <takabatoshiki@gmail.com>
2018-10-24 08:27:52 +0900

    add search_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def search
+    @users = User.where('name LIKE(?)', "#{params[:user_name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email)
