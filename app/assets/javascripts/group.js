6078c677c8f07b5d00184c27eeb0037991a39850
takabatoshiki <takabatoshiki@gmail.com>
2018-10-25 08:03:16 +0900

    add user_search_result to group.js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,4 +1,14 @@
 $(function() {
+  var user_search_result = $('#user-search-result');
+
+  function appendUserName(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.user_name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.user_name}">追加</a>
+                </div>`
+    user_search_result.append(html);
+  }
+
   $("#group_name").on("keyup", function() {
     var input = $("#group_name").val();
     console.log(input);
@@ -12,6 +22,14 @@ $(function() {
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
+    })
+    .done(function(users){
+      $('#user-search-result').empty();
+      if (users.length != 0){
+        users.forEach(function(user){
+          appendUserName(user);
+        });
+      }
     })
   });
 });
