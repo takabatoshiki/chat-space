bc5c281bae4e64df211bdeb918d9283fcad8b4f1
takabatoshiki <takabatoshiki@gmail.com>
2018-10-24 09:08:38 +0900

    change def_search to def_index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where('name LIKE(?)', "#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
@@ -11,14 +16,6 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit
-    end
-  end
-
-  def search
-    @users = User.where('name LIKE(?)', "#{params[:keyword]}%")
-    respond_to do |format|
-      format.html
-      format.json
     end
   end
 
