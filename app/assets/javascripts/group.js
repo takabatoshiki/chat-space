2a6118d9ade7f68d1e9a00649d0a44a32c9a8a38
takabatoshiki <takabatoshiki@gmail.com>
2018-10-25 07:03:10 +0900

    add input_event to gourp.js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,1 +1,11 @@
+$(function() {
+  $("#group_name").on("keyup", function() {
+    var input = $("#group_name").val();
+    console.log(input);
+  });
 
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    console.log(input);
+  });
+});
