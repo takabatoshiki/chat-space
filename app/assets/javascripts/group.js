ca21df3c6691629c193f25184071da848e08c891
takabatoshiki <takabatoshiki@gmail.com>
2018-10-27 13:18:21 +0900

    delete grouop_name keyup

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -9,11 +9,6 @@ $(function() {
     user_search_result.append(html);
   }
 
-  $("#group_name").on("keyup", function() {
-    var input = $("#group_name").val();
-    console.log(input);
-  });
-
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     console.log(input);
