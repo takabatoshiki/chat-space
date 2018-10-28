3e6172f03b111e73aa562c149bd0753830f654bb
takabatoshiki <takabatoshiki@gmail.com>
2018-10-28 09:09:53 +0900

    change user-search-add  @group.js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -35,9 +35,7 @@ $(function() {
   });
 
   $('#user-search-result').on('click', '.user-search-add',function(){
-    var user_id = $('.chat-group-user__btn--add').attr('data-user-id');
-    var user_add = $('.chat-group-user__btn--add').attr('data-user-name');
-    console.log(user_id);
-    console.log(user_add);
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
   });
 });
