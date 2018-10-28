d56393fcbe8b15f2ec80f5a8fc7fee4c325364e6
takabatoshiki <takabatoshiki@gmail.com>
2018-10-28 09:14:52 +0900

    add chat-group-users to group.js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,5 +1,6 @@
 $(function() {
   var user_search_result = $('#user-search-result');
+  var chat_group_users = $('#chat-group-users');
 
   function appendUserName(user){
     var html = `<div class="chat-group-user clearfix">
@@ -7,6 +8,15 @@ $(function() {
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     user_search_result.append(html);
+  }
+
+  function appendGroupUser(user_id, user_name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    chat_group_users.append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
@@ -37,5 +47,6 @@ $(function() {
   $('#user-search-result').on('click', '.user-search-add',function(){
     var user_id = $(this).attr('data-user-id');
     var user_name = $(this).attr('data-user-name');
+    appendGroupUser(user_id, user_name);
   });
 });
