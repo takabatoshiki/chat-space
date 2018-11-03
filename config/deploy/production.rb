32eea5904f1be75a151773c9a2fc42c3fe2f0dd1
takabatoshiki <takabatoshiki@gmail.com>
2018-11-03 18:18:29 +0900

    add ip & user to production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server "52.193.160.155", user: "ec2-user", roles: %w{app db web}
 
 # role-based syntax
 # ==================
